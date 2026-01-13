776f0b00b1648f2080ff43dd01ad253753c9e5db
zieeco <zieecosam@gmail.com>
2026-01-13 04:38:53 +0100

    feat: update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import ChatWidget from "@/components/ChatWidget";
 import AdminInitCheck from "@/components/auth/AdminInitCheck";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -26,15 +23,8 @@ export default function RootLayout({
       <body
         className={`${plusJakartaSans.variable} antialiased text-slate-800`}
       >
-        <div className="relative mesh-gradient min-h-screen flex flex-col">
-          <Header />
-            <main className="transition-opacity duration-500 flex-grow">
-              {children}
-            </main>
-          <Footer />
-          <ChatWidget />
+          {children}
           <AdminInitCheck />
-        </div>
       </body>
     </html>
   );
