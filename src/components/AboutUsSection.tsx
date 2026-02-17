7c1e7254ee14c7c2c28a47474183d442c31ed7d8
zieeco <zieecosam@gmail.com>
2026-02-17 15:28:20 +0100

    feat: update width

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -18,7 +18,7 @@ const AboutUsSection: React.FC = () => {
             <div className="absolute top-0 right-0 w-1/3 h-full bg-brand-gold/5 blur-[120px] rounded-full translate-x-1/2"></div>
             <div className="absolute bottom-0 left-0 w-1/4 h-1/2 bg-blue-500/5 blur-[100px] rounded-full -translate-x-1/2"></div>
 
-            <div className="max-w-7xl mx-auto px-6 md:px-12 lg:px-24 relative z-10">
+            <div className="max-w-[1440px] mx-auto w-full relative z-10 px-6 md:px-16 lg:px-24">
                 <div className="grid lg:grid-cols-2 gap-16 items-center">
                     {/* Left Column: Heading */}
                     <div className="flex flex-col">
