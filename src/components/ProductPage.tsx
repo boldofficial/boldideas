819fe3c51a13ff17f42b574f3376011020370ff9
zieeco <zieecosam@gmail.com>
2026-02-18 17:01:06 +0100

    feat: update component

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import ServicesHero from './services/ServicesHero';
-import ServicesList from './services/ServicesList';
-import ServicesAdvantage from './services/ServicesAdvantage';
+import ProductHero from './products/ProductHero';
+import ProductDetails from './products/ProductDetails';
+import { Product } from '@/data/product';
 
-const ServicesPage: React.FC = () => {
+interface ProductsPageProps {
+  product?: Product;
+}
+
+const ProductPage: React.FC<ProductsPageProps> = ({ product }) => {
   return (
     <div className="pt-24 pb-20 overflow-hidden bg-brand-light relative">
       {/* Schematic Grid Background */}
@@ -11,12 +15,12 @@ const ServicesPage: React.FC = () => {
            style={{ backgroundImage: 'linear-gradient(#002D5B 1px, transparent 1px), linear-gradient(90deg, #002D5B 1px, transparent 1px)', backgroundSize: '40px 40px' }}>
       </div>
 
-      <ServicesHero />
-      <ServicesList />
-      <ServicesAdvantage />
+      <ProductHero product={product} />
+      {product && <ProductDetails product={product} />}
+      {/* <ServicesList /> */}
     </div>
   );
 };
 
-export default ServicesPage;
+export default ProductPage;
 
